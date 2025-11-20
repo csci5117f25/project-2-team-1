6003d727855a07d900ab8647476b36a43cd3eaf0
ConnellHagen <hagen.connell@gmail.com>
2025-11-19 18:06:52 -0600

    Create Routes and Components

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import HomeView from "@/views/HomeView.vue";
+import LoginView from "@/views/LoginView.vue";
+import SettingsView from "@/views/SettingsView.vue";
+import StatsView from "@/views/StatsView.vue";
 import Error404View from "@/views/404View.vue";
 
 const router = createRouter({
@@ -12,8 +15,24 @@ const router = createRouter({
       meta: { requiresAuth: false },
     },
     {
+      path: "/login",
+      component: LoginView,
+      meta: { requiresAuth: false },
+    },
+    {
+      path: "/settings",
+      component: SettingsView,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: "/stats",
+      component: StatsView,
+      meta: { requiresAuth: true },
+    },
+    {
       path: "/404",
       component: Error404View,
+      meta: { requiresAuth: false },
     },
     {
       path: "/:catchAll(.*)",
