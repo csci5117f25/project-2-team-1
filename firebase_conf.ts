19b43557c5e7f67285008fdeaa7763334b1973a5
ConnellHagen <hagen.connell@gmail.com>
2025-11-19 18:56:16 -0600

    Add Login/Out

--- firebase_conf.ts
+++ firebase_conf.ts
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAUsn_hkxg7FmiQPt89s-r8S24A4MDIlVk",
   authDomain: "gyst-1ba35.firebaseapp.com",
@@ -14,6 +10,5 @@ const firebaseConfig = {
   appId: "1:483812494206:web:cbbab26bfdea6d5868012f"
 };
 
-// Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
 export const db = getFirestore(firebaseApp);
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,22 +1,41 @@
 <script setup lang="ts">
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
-import { useCurrentUser, useFirebaseAuth } from 'vuefire'
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { useCurrentUser, useFirebaseAuth } from "vuefire";
 
-const user = useCurrentUser()
-const auth = useFirebaseAuth()
-const provider = new GoogleAuthProvider()
+const user = useCurrentUser();
+const auth = useFirebaseAuth();
+const provider = new GoogleAuthProvider();
 
 async function login() {
-    try {
-        const result = await signInWithPopup(auth, provider)
-    } catch {
-        alert("oh no");
-    }
+  if (!auth) {
+    return;
   }
+
+  try {
+    await signInWithPopup(auth, provider);
+  } catch {
+    alert("oh no");
+  }
+}
+
+async function logout() {
+  if (!auth) {
+    return;
+  }
+
+  try {
+    await signOut(auth);
+  } catch {
+    alert("oh no")
+  }
+}
 </script>
 
 <template>
-  <div><button @click="login()" v-if="!user">log in</button></div>
+  <div>
+    <button v-if="!user" @click="login()">Log In</button>
+    <button v-else @click="logout()">Log Out</button>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
