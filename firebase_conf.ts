2c8c4b56116b69d2b74677e123cdf57351d19ddf
Daigan Berger <daiganberger@gmail.com>
2025-11-19 18:19:14 -0600

    conf

---
+++ firebase_conf.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAUsn_hkxg7FmiQPt89s-r8S24A4MDIlVk",
+  authDomain: "gyst-1ba35.firebaseapp.com",
+  projectId: "gyst-1ba35",
+  storageBucket: "gyst-1ba35.firebasestorage.app",
+  messagingSenderId: "483812494206",
+  appId: "1:483812494206:web:cbbab26bfdea6d5868012f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
